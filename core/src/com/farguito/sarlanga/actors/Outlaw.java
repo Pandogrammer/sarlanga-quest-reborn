5e61f042d3199eac663b443be364c887420fb858
Latharia ~ <Latharia ~>
2016-12-13 09:32:07 -0300

    fix de bichos

--- core/src/com/farguito/sarlanga/actors/Outlaw.java
+++ core/src/com/farguito/sarlanga/actors/Outlaw.java
@@ -7,7 +7,7 @@ public class Outlaw extends Character {
     private static int hp = 40;
     private static int damage = 15;
     private static int defense = 5;
-    private static float speed = 21f;
+    private static float speed = 1f;
     private static int minTurnCounter = 2;
     private static int maxTurnCounter = 5;
 
--- core/src/com/farguito/sarlanga/actors/Rat.java
+++ core/src/com/farguito/sarlanga/actors/Rat.java
@@ -4,7 +4,7 @@ import static com.farguito.sarlanga.helpers.TextureHelper.CharacterCode.RAT;
 
 public class Rat extends Character {
 
-    private static int hp = 1;
+    private static int hp = 20;
     private static int damage = 10;
     private static int defense = 3;
     private static float speed = 1.5f;
